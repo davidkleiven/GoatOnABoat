b8d849536d3e650eeb4baeb154e28d0e106ec821
David Kleiven <davidkleiven446@gmail.com>
2018-02-10 14:36:06 +0100

    Added loop that tries multiple times on web questions

--- gob/bird_question.py
+++ gob/bird_question.py
@@ -12,7 +12,8 @@ class BirdQuestion( QuestionWithImage ):
         self.birds = []
         with open( settings.bird_file, 'r' ) as infile:
             self.birds = [line.rstrip().decode("utf8").lower() for line in infile.readlines()]
-        print (self.birds)
+        self.max_attempts = 40
+        self.current_attempts = 0
 
     def get_url( self, bird_name ):
         #url = "https://www.google.no/search?q="+bird_name+"&dcr=0&source=lnms&tbm=isch&sa=X&ved=0ahUKEwid5tHXk5vZAhUG1SwKHTihBLkQ_AUICigB&biw=1920&bih=939"
@@ -20,21 +21,29 @@ class BirdQuestion( QuestionWithImage ):
         return url
 
     def get( self ):
-        random.shuffle( self.birds )
-        res = requests.get( self.get_url(self.birds[0]) )
-        print (self.birds[0])
-        soup = BeautifulSoup( res.text )
-        question = "Hvilken fugl?"
+        try:
+            random.shuffle( self.birds )
+            res = requests.get( self.get_url(self.birds[0]) )
+            soup = BeautifulSoup( res.text )
+            question = "Hvilken fugl?"
 
-        # Get all images
-        #imgs = soup.findAll( "div", {"class":"rg_meta notranslate"} )
-        imgs = soup.findAll( "figure", {"class":"image landscape non-expandable published"} )
-        imgs += soup.findAll( "figure", {"class":"image portrait non-expandable published"})
-        link = imgs[0].img["src"]
-        #link = json.loads( imgs[0].text )["ou"]
-        fname = link.split("/")[-1]
-        self.image_name = "tmp/"+fname
-        wget.download( link, out=self.image_name )
+            # Get all images
+            imgs = soup.findAll( "figure", {"class":"image landscape non-expandable published"} )
+            imgs += soup.findAll( "figure", {"class":"image portrait non-expandable published"})
+            link = imgs[0].img["src"]
+            #link = json.loads( imgs[0].text )["ou"]
+            fname = link.split("/")[-1]
+            self.image_name = "tmp/"+fname
+            wget.download( link, out=self.image_name )
 
-        alternatives = self.birds[:4]
-        return question, alternatives
+            alternatives = self.birds[:4]
+            self.current_attempts = 0
+            return question, alternatives
+        except Exception as exc:
+            print ("Bird question: "+str(exc) )
+            self.current_attempts += 1
+            if ( self.current_attempts < self.max_attempts ):
+                self.get()
+            else:
+                raise RuntimeError( "Could not create bird question!" )
+                self.current_attempts = 0
--- gob/famous_norwegians.py
+++ gob/famous_norwegians.py
@@ -10,6 +10,8 @@ class FamousNorwegians( QuestionWithImage ):
     def __init__( self ):
         QuestionWithImage.__init__(self)
         self.names =[]
+        self.max_attempts = 40
+        self.current_attempts = 0
         with open( settings.famous_norw_file , 'r' ) as infile:
             for line in infile.readlines():
                 line = line.decode("utf8")
@@ -22,41 +24,51 @@ class FamousNorwegians( QuestionWithImage ):
         return url
 
     def get( self ):
-        random.shuffle( self.names )
-        url = self.get_url(self.names[0])
-        res = requests.get( url )
-        soup = BeautifulSoup( res.text )
-        res = soup.findAll( "table", {"class":re.compile("infobox*")})
-        if ( len(res) == 0 ):
-            raise ValueError( "No infobox on wikipedia page!" )
-        #soup = BeautifulSoup( res[0] )
-        imgs = res[0].findAll( "a", {"class":"image"} )
-        link = imgs[0].img["src"]
+        try:
+            random.shuffle( self.names )
+            url = self.get_url(self.names[0])
+            res = requests.get( url )
+            soup = BeautifulSoup( res.text )
+            res = soup.findAll( "table", {"class":re.compile("infobox*")})
+            if ( len(res) == 0 ):
+                raise ValueError( "No infobox on wikipedia page!" )
+            #soup = BeautifulSoup( res[0] )
+            imgs = res[0].findAll( "a", {"class":"image"} )
+            link = imgs[0].img["src"]
 
-        # Find extension in string
-        extensions = ["jpg","png"]
-        ext_found = False
-        for ext in extensions:
-            pos = link.find(ext)
-            if ( pos != -1 ):
-                ext_found = True
-                link = link[:pos]+ext
-                break
-        if ( not ext_found ):
-            raise ValueError( "Could not one of the file extensions {}".format(extensions) )
+            # Find extension in string
+            extensions = ["jpg","png"]
+            ext_found = False
+            for ext in extensions:
+                pos = link.find(ext)
+                if ( pos != -1 ):
+                    ext_found = True
+                    link = link[:pos]+ext
+                    break
+            if ( not ext_found ):
+                raise ValueError( "Could not one of the file extensions {}".format(extensions) )
 
-        splitted_link = link.split("/")
-        if ( "thumb" in splitted_link ):
-            # Remove the thumb
-            indx = splitted_link.index("thumb")
-            del splitted_link[indx]
-        link = ""
-        for token in splitted_link:
-            link += token+"/"
-        link = link[:-1] # Remove the last backslash
-        link = "https:"+link
-        fname = link.split("/")[-1]
-        self.image_name = "tmp/"+fname
-        wget.download( link, out=self.image_name )
-        text = "Hvilken norsk kjendis?"
-        return text, self.names[:4]
+            splitted_link = link.split("/")
+            if ( "thumb" in splitted_link ):
+                # Remove the thumb
+                indx = splitted_link.index("thumb")
+                del splitted_link[indx]
+            link = ""
+            for token in splitted_link:
+                link += token+"/"
+            link = link[:-1] # Remove the last backslash
+            link = "https:"+link
+            fname = link.split("/")[-1]
+            self.image_name = "tmp/"+fname
+            wget.download( link, out=self.image_name )
+            text = "Hvilken norsk kjendis?"
+            self.current_attempts = 0
+            return text, self.names[:4]
+        except Exception as exc:
+            print ("Famour norw: "+str(exc))
+            self.current_attempts += 1
+            if ( self.current_attempts < self.max_attempts ):
+                self.get()
+            else:
+                self.current_attempts = 0
+                raise RuntimeError( "Could not find question in {} attempts".format(self.max_attempts))
